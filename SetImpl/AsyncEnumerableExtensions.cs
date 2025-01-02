18182748dadef4811dcca3006db1217a7e303f7c
Joe Warwick <jrwarwick@hotmail.com>
2025-01-03 08:54:15 +1100

    async again

---
+++ SetImpl/AsyncEnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿public static class AsyncEnumerableExtensions
+{
+    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default)
+    {
+        var list = new List<T>();
+        await foreach (var item in source.WithCancellation(cancellationToken))
+        {
+            list.Add(item);
+        }
+        return list;
+    }
+}
