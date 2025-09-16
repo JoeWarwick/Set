a7137ceb5ced458f6ffbd3de50df3d007787f103
Joe Warwick <jrwarwick@hotmail.com>
2025-09-16 18:18:41 +1000

    More complex object type test

--- SetImpl/Program.cs
+++ SetImpl/Program.cs
@@ -4,19 +4,46 @@
     {
         static void Main()
         {
-            AVLTree<int> tree = new AVLTree<int>();
+            AVLTree<Volume> tree = new();
 
-            tree.Insert(10);
-            tree.Insert(20);
-            tree.Insert(30);
-            tree.Insert(40);
-            tree.Insert(50);
-            tree.Insert(25);
+            tree.Insert(new Volume(1, "C1", 1223, 324));
+            tree.Insert(new Volume(5, "C5", 1234656, 23636));
+            tree.Insert(new Volume(3, "F", 34734, 23465));
+            tree.Insert(new Volume(6, "B", 3245, 122));
+            tree.Insert(new Volume(2, "A", 2145, 212));
+            tree.Insert(new Volume(4, "D", 314245, 21354));
 
-            var query = tree.Where(x => x > 20);
+            var query = tree.Where(x => x.Free > 2800);
             foreach (var item in query)
             {
                 Console.WriteLine(item);
+            }
+        }
+
+        class Volume : IComparable<Volume>
+        {
+            public Volume(int id, string name, int size, int used)
+            {
+                Id = id;
+                Name = name;
+                Size = size;
+                Used = used;
+            }
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Size { get; set; }
+            public int Used { get; set; }
+            public int Free => Size - Used;
+            public override string ToString()
+            {
+                return $"{Id}: {Name} ({Size} - {Used})";
+            }
+
+            public int CompareTo(Volume? other)
+            {
+                if (other == null) return 1;
+                return Size.CompareTo(other.Size);
             }
         }
     }
